f6579eee9cc6654fa6b6eab3ce589e5a7c7895bb
Thorgarch <thorgalblanco@gmail.com>
2024-06-04 11:00:17 +0200

    removed unused functions

--- src/fas/feedback-arcset-heuristic.cpp
+++ src/fas/feedback-arcset-heuristic.cpp
@@ -22,39 +22,8 @@
  * Contact: tbl007@uib.no
  */
 
-
-
-/** TODO:
- * 1. Maybe unclude hsum for calculating crossings.
- * 2. PERSONAL: look at perf doe rime checking
- */
-
-
-
 // Helper functions
 //
-//
-
-/* Given a 32x32 matrix, we set its edge-weights to random values for a given amount of nodes.
- * 
- * @param num_nodes         - Amount of nodes the graph has.
- * @param (*matrix)[32][32] - Pointer to the matrix of which values to update.
- */
-
-void matrix_32_gen(unsigned int num_nodes, unsigned int (*matrix)[32][32], unsigned int *num_nodes_ref, unsigned int seed){
-    unsigned int rand_int;
-    *num_nodes_ref = num_nodes;
-    if(seed == 0){std::srand(time(NULL));}
-    else {std::srand(seed);};
-    for (int i = 0; i< num_nodes; i++){
-        for (int j = 0; j < num_nodes; j++){
-            rand_int = 0 + std::rand() % 10;
-            if (!(i == j)){
-                (*matrix)[i][j] = rand_int;
-            }
-        };
-    };
-}
 
 void matrix_n_gen(unsigned int num_nodes, int **&matrix, unsigned int seed){
     unsigned int rand_int;
@@ -69,54 +38,6 @@ void matrix_n_gen(unsigned int num_nodes, int **&matrix, unsigned int seed){
         };
     };
 }
-
-
-void initialize_matrix(unsigned int (*matrix)[32][32]){
-    for (int i = 0; i < 32; i++){
-        for (int j = 0; j < 32; j++){
-            (*matrix)[i][j] = 0;
-        };
-    };
-}
-
-void test_matrix_1(unsigned int (*matrix)[32][32], unsigned int *num_nodes){
-    *num_nodes = 6;
-    initialize_matrix(&(*matrix));
-    (*matrix)[0][1] = 3;(*matrix)[1][3] = 2;(*matrix)[1][4] = 4;(*matrix)[2][1] = 10;(*matrix)[2][5] = 4;
-    (*matrix)[3][0] = 2;(*matrix)[3][1] = 1;(*matrix)[4][3] = 5;(*matrix)[4][5] = 1;(*matrix)[5][2] = 7;
-};
-
-void test_matrix_2(unsigned int (*matrix)[32][32], unsigned int *num_nodes){
-    *num_nodes = 10;
-    initialize_matrix(&(*matrix));
-    (*matrix)[0][1] = 5;(*matrix)[0][2] = 8;
-    (*matrix)[0][3] = 3; (*matrix)[1][3] = 8;(*matrix)[2][4] = 5;(*matrix)[3][0] = 1;(*matrix)[3][1] = 7;(*matrix)[4][3] = 26;
-    (*matrix)[4][5] = 7;(*matrix)[5][7] = 2;(*matrix)[6][4] = 8;(*matrix)[6][5] = 3;(*matrix)[7][9] = 15;(*matrix)[8][7] = 11;(*matrix)[9][8] = 23;
-};
-
-
-/* calculates n choose k
- *
- * @param n - number of elements to be distributed
- * @param k - space to distribute the elements
- */
-unsigned long long choose(unsigned int n, unsigned int k){
-
-    if ( k > n ) {return 0; };
-
-    if ( k == 0 ) {return 1; };
-
-    unsigned long long mult = n;
-    unsigned long long div = k;
-
-    for ( unsigned int i = 1; i < k; i++ ){
-        mult *= (n - i);
-        div *= (k - i);
-    };
-    unsigned long long result = mult/div;
-
-    return result;
-};
 
 // Important functions
 //
@@ -133,9 +54,7 @@ unsigned long long choose(unsigned int n, unsigned int k){
  */
 unsigned long long count_crossings(int k, unsigned long long nodes, int node_count,
                              std::vector<std::vector<int>> &matrix){
-
     unsigned int count = 0;
-
     for (unsigned int i = 0; i < node_count; i++){ 
         if (nodes & (1 << i)){
             if ( matrix[k][i] > 0 ){// Checks wether any of the included nodes are in k's adj_list (adj_map)
@@ -173,7 +92,6 @@ unsigned long long cum_crossings(unsigned long long curr_itt, unsigned int num_n
             curr_perm = curr_perm ^ (unsigned long long) std::pow(2, i); // We exclude curr_node from curr_perm
             curr_sum = DP[curr_perm] + count_crossings(i, curr_perm, num_nodes, matrix);
 
-
             if (sum_initiallized){// This is the DP recurrsion
                 result = std::min(curr_sum, result);
             }
@@ -183,9 +101,7 @@ unsigned long long cum_crossings(unsigned long long curr_itt, unsigned int num_n
             };
         };
     };
-
     return result;
-
 };
 
 int next_in_ordering(std::vector<unsigned long long> &DP, std::vector<std::vector<int>> &Q,
@@ -198,10 +114,8 @@ int next_in_ordering(std::vector<unsigned long long> &DP, std::vector<std::vecto
             return i;
         };
     };
-    std::cout << "Got to somewhere we shouldnt be";
     return -1; //Should never reach this stage
-
-    };
+};
 
 std::vector<int> final_ordering(std::vector<unsigned long long> &DP, std::vector<std::vector<int>> &matrix,
                                     int num_nodes){
@@ -225,19 +139,6 @@ std::vector<int> final_ordering(std::vector<unsigned long long> &DP, std::vector
     return ordering;
 };
 
-
-void write_results(unsigned int num_nodes, double time){
-    std::ofstream outfile;
-    outfile.open("log.txt", std::ios_base::app);
-    //outfile << "ALGORITHM 2 RESULTS: " << "\n";
-    outfile << "number of nodes: " << num_nodes << ", time spent: " << time << std::endl;
-};
-
-
-// main
-//
-//
-
 int crossings_in_order(int *S, int **matrix, int num_nodes){
     int count = 0;
     for (int i = num_nodes-1; i >= 0; i--){ 
@@ -256,22 +157,6 @@ int crossings_in_order(int *S, int **matrix, int num_nodes){
  *
  */
 void heuristic_dp_ordering(int **W, int *S, int n, int q){
-    /* ALGORITHM:
-     * 1. select last q nodes of S, save in qxq-matrix Q.
-     * 2. Calculate DP-table of Q, in table DP
-     * 3. run final_ordering, only for first element, and prepend corresponding node s_old to new ordering S'
-     * 4. Swap s_old for next element s_new in S
-     * 5. Swap s_old for s_new in Q
-     * 6. repeat until #nodes left in S = q
-     * 7. If S' <= S in amount of crossings, set S = S'.
-     * 
-     * OPTIMIZATIONS:
-     * - In 2, if not on first itteration, only update DP-table where s_old was included.
-     * - Maybe not remake/make new matrix Q, but have it point to W-values.
-     *
-     * NOTES:
-     * - Currently assuming matrix Q max size is 32x32
-     */
     int remaining_nodes = n;
     int S_prime[n];
     int q_window[std::min(n, q)];
@@ -316,10 +201,6 @@ void heuristic_dp_ordering(int **W, int *S, int n, int q){
             Q[next_swap_idx][i] = W[q_window[next_swap_idx]][q_window[i]];
             Q[i][next_swap_idx] = W[q_window[i]][q_window[next_swap_idx]];
         };
-
-
-
-
     };
 };
 
@@ -382,37 +263,4 @@ void heur_test(){
 int main(int argc, char * argv[]){
     std::cout << "Started main\n";
     heur_test();
-
-
-/*
-    auto start_time = std::chrono::high_resolution_clock::now();//timing
-
-    unsigned int num_nodes;
-    unsigned int seed;
-    num_nodes = atoi(argv[1]);
-    seed = atoi(argv[2]);
-    unsigned int matrix[32][32];
-    matrix_32_gen(num_nodes, &matrix, &num_nodes, seed);
-
-    std::vector<unsigned long long> DP(1<<num_nodes);
-    DP[0] = 0;
-
-    for (unsigned int n = 1; n < (1<<num_nodes); n++){
-        DP[n] = cum_crossings(n, num_nodes, &DP, &matrix);
-    };
-
-
-    for (unsigned int j : final_ordering(&DP, &matrix, num_nodes)){
-        //std::cout << j << " ";
-    };
-
-
-    auto end_time = std::chrono::high_resolution_clock::now();//timing
-    double time_spent = ((std::chrono::duration<double, std::ratio<1>>)(end_time - start_time)).count();
-
-
-    //std::cout << "\n" << DP[(1<<num_nodes) - 1];
-    //std::cout << "\n";
-    write_results(num_nodes, time_spent);
-    */
 };
